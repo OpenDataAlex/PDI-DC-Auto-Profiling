9c547f02c6db11ce2ac7593cc00a0f83a0d7850e
Alex Meadows <alexmeadows@bluefiredatasolutions.com>
2013-02-13 14:20:41 -0500

    Update database/profile_unkown_record.sql
    Resolving remaining issue with the script being out of sync with the current version of the database

--- database/profile_unkown_record.sql
+++ database/profile_unkown_record.sql
@@ -29,54 +29,51 @@ WHERE profile_source_table_name = 'unknown'
 LIMIT 1;
 
 INSERT INTO profile_source_table_number_analysis
-(profile_source_table_column_pk, table_column_name, w_create_dt)
+(profile_source_table_column_pk, w_create_dt)
 VALUES
-(0, 'unknown', NOW());
+(0, NOW());
 
 UPDATE profile_source_table_number_analysis
 SET profile_source_table_number_analysis_pk = 0
-WHERE profile_source_table_pk = 0
-  AND table_column_name = 'unknown'
+WHERE profile_source_table_column_pk = 0
 LIMIT 1;
 
 INSERT INTO profile_source_table_pattern_finder_analysis
-(profile_source_table_column_pk, table_column_name, w_create_dt)
+(profile_source_table_column_pk, w_create_dt)
 VALUES
-(0, 'unknown', NOW());
+(0, NOW());
 
 UPDATE profile_source_table_pattern_finder_analysis
 SET profile_source_table_pattern_finder_analysis_pk = 0
-WHERE profile_source_table_pk = 0
-  AND table_column_name = 'unknown'
+WHERE profile_source_table_column_pk = 0
 LIMIT 1;
 
 INSERT INTO profile_source_table_string_analysis
-(profile_source_table_column_pk, table_column_name, w_create_dt)
+(profile_source_table_column_pk, w_create_dt)
 VALUES
-(0, 'unknown', NOW());
+(0, NOW());
 
 UPDATE profile_source_table_string_analysis
 SET profile_source_table_string_analysis_pk = 0
-WHERE profile_source_table_pk = 0
-  AND table_column_name = 'unknown';
+WHERE profile_source_table_column_pk = 0
+LIMIT 1;
 
 INSERT INTO profile_source_table_value_distribution_analysis
-(profile_source_table_column_pk, table_column_name, w_create_dt)
+(profile_source_table_column_pk, w_create_dt)
 VALUES
-(0, 'unknown', NOW());
+(0, NOW());
 
 UPDATE profile_source_table_value_distribution_analysis
 SET profile_source_table_value_distribution_analysis_pk = 0
-WHERE profile_source_table_pk = 0
-  AND table_column_name = 'unknown';
+WHERE profile_source_table_column_pk = 0
+LIMIT 1;
 
 INSERT INTO profile_source_table_value_distribution_summary_analysis
-(profile_source_table_column_pk, table_column_name, w_create_dt)
+(profile_source_table_column_pk, w_create_dt)
 VALUES
-(0, 'unknown', NOW());
+(0, NOW());
 
 UPDATE profile_source_table_value_distribution_summary_analysis
 SET profile_source_table_value_distribution_summary_analysis_pk = 0
-WHERE profile_source_table_pk = 0
-  AND table_column_name = 'unknown'
+WHERE profile_source_table_column_pk = 0
 LIMIT 1;
